eb9ff07180ed00b8aa9f442fba07085b75187c5e
Rahul Kumar Pandit <55670332+rahulfrndz02@users.noreply.github.com>
2023-06-17 15:32:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+function removeColor() {
+    var select = document.getElementById("colorSelec");
+	var selectedOption = select.options[select.selectedIndex];
+	select.removeChild(selectedOption);
+} 
+
+// window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
